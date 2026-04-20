af572bd3624087bbd0960ffaadce495aaacbfbc4
JustADude2 <sageda1@s.dcsdk12.org>
2026-04-20 15:44:41 -0600

    added a coin, added something to player that I forgot

--- scenes/items/coin.gd
+++ scenes/items/coin.gd
@@ -3,6 +3,7 @@ extends Area2D
 signal collected
 
 func _on_body_entered(body: Node) -> void:
+	print("e")
 	if body.is_in_group("player"):
 		collected.emit()
-		queue_free()
+		%AnimationPlayer.play("collect_coin")
